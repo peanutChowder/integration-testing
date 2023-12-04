e46a489db8c432696b176a8db46644385c95413e
Jacob Feng <peanutChowder@gmail.com>
2023-12-03 20:06:10 -0700

    Initial commit

---
+++ tests-bottomup/TestRunner.py
@@ -0,0 +1,33 @@
+# tests/TestRunner.py
+#The basic idea here is to have a new Python file Testrunner.py
+# alongside your tests that contains our runner.
+import unittest
+
+# # import your tests modules (Apply integration testing method)
+# from tests import TestModuleA
+# from tests import TestModuleB
+# from tests import TestModuleC
+# from tests import TestModuleD
+# from tests import TestModuleE
+# from tests import TestModuleF
+# from tests import TestModuleG
+
+
+
+# # initialize the tests suite
+# loader = unittest.TestLoader()
+# suite  = unittest.TestSuite()
+
+# # add tests to the tests suite
+# suite.addTests(loader.loadTestsFromModule(TestModuleA))
+# suite.addTests(loader.loadTestsFromModule(TestModuleB))
+# suite.addTests(loader.loadTestsFromModule(TestModuleC))
+# suite.addTests(loader.loadTestsFromModule(TestModuleD))
+# suite.addTests(loader.loadTestsFromModule(TestModuleE))
+# suite.addTests(loader.loadTestsFromModule(TestModuleF))
+# suite.addTests(loader.loadTestsFromModule(TestModuleG))
+
+
+# # initialize a runner, pass it your suite and run it
+# runner = unittest.TextTestRunner(verbosity=3)
+# result = runner.run(suite)
