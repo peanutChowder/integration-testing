dba3e2a444533069a5c61c5b8757f6eb7c4f540c
Jacob Feng <peanutChowder@gmail.com>
2023-12-04 15:14:26 -0700

    Completed tests

--- tests-bottomup/TestModuleD.py
+++ tests-bottomup/TestModuleD.py
@@ -53,68 +53,100 @@ class TestModuleD(unittest.TestCase):
 
         self.assertEqual(returnVal, dummyData)
 
-    # def test_update_data(self):
-    #     initialData = [
-    #         Entry("data1", "1"),
-    #         Entry("data2", "2"),
-    #         Entry("data3", "3")
-    #     ]
+    @patch("builtins.print")
+    @patch("builtins.open", new_callable=mock_open)
+    def test_update_data(self, mockOpen: MagicMock, mockPrint: MagicMock):
+        initialData = [
+            Entry("data1", "1"),
+            Entry("data2", "2"),
+            Entry("data3", "3")
+        ]
 
-    #     finalData = [
-    #         Entry("data1", "1"),
-    #         Entry("newData", "4"),
-    #         Entry("data3", "3")
-    #     ]
+        finalData = [
+            Entry("data1", "1"),
+            Entry("newData", "4"),
+            Entry("data3", "3")
+        ]
 
-    #     self.moduleD.updateData(
-    #         initialData, 1,
-    #         "newDataName", "2",
-    #         "file.txt"
-    #     )
+        self.moduleD.updateData(
+            initialData, 1,
+            "newData", "4",
+            "file.txt"
+        )
 
-    #     for i in range(len(initialData)):
-    #         self.assertEqual(initialData[i], finalData[i])
+        for i in range(len(initialData)):
+            self.assertEqual(initialData[i], finalData[i])
         
-    #     self.mockF.displayData.assert_called_once_with(finalData)
+        mockPrint.assert_has_calls([
+            call("Current Data:"),
+            call("----------------------------------------------------------"),
+            call("1 data1, 1"),
+            call("2 newData, 4"),
+            call("3 data3, 3"),
+            call("----------------------------------------------------------")
+        ])
 
-    #     self.mockG.assert_called_once_with("file.txt", finalData)
+        file = mockOpen("file.txt", "w")
 
-    # def test_delete_data(self):
-    #     initialData = [
-    #         Entry("data1", "1"),
-    #         Entry("data2", "2"),
-    #         Entry("data3", "3")
-    #     ]
-    #     finalData = [
-    #         Entry("data1", "1"),
-    #         Entry("data2", "2"),
-    #     ]
+        file.write.assert_has_calls([
+            call("data1,1\n"),
+            call("newData,4\n"),
+            call("data3,3\n")
+        ])
 
-    #     self.moduleD.deleteData(
-    #         initialData, 2, "file.txt"
-    #     )
+    @patch("builtins.print")
+    @patch("builtins.open", new_callable=mock_open)
+    def test_delete_data(self, mockOpen: MagicMock, mockPrint: MagicMock):
+        initialData = [
+            Entry("data1", "1"),
+            Entry("data2", "2"),
+            Entry("data3", "3")
+        ]
+        finalData = [
+            Entry("data1", "1"),
+            Entry("data2", "2"),
+        ]
 
-    #     self.assertEqual(len(initialData), len(finalData))
+        self.moduleD.deleteData(
+            initialData, 2, "file.txt"
+        )
 
-    #     for i in range(len(initialData)):
-    #         self.assertEqual(initialData[i], finalData[i])
+        self.assertEqual(len(initialData), len(finalData))
 
-    # def test_f_getter(self):
-    #     returnVal = self.moduleD.f
+        for i in range(len(initialData)):
+            self.assertEqual(initialData[i], finalData[i])
 
-    #     self.assertEqual(returnVal, self.mockF)
+        mockPrint.assert_has_calls([
+            call("Current Data:"),
+            call("----------------------------------------------------------"),
+            call("1 data1, 1"),
+            call("2 data2, 2"),
+            call("----------------------------------------------------------")
+        ])
 
-    # def test_g_getter(self):
-    #     returnVal = self.moduleD.g
+        file = mockOpen("file.txt", "w")
 
-    #     self.assertEqual(returnVal, self.mockG)
+        file.write.assert_has_calls([
+            call("data1,1\n"),
+            call("data2,2\n"),
+        ])
 
-    # def test_f_setter(self):
-    #     self.moduleD.f = "f"
+    def test_f_getter(self):
+        returnVal = self.moduleD.f
 
-    #     self.assertEqual(self.moduleD._f, "f")
+        self.assertEqual(returnVal, self.moduleF)
 
-    # def test_g_setter(self):
-    #     self.moduleD.g = "g"
+    def test_g_getter(self):
+        returnVal = self.moduleD.g
 
-    #     self.assertEqual(self.moduleD.g, "g")
+        self.assertEqual(returnVal, self.moduleG)
+
+    def test_f_setter(self):
+        self.moduleD.f = "f"
+
+        self.assertEqual(self.moduleD._f, "f")
+
+    def test_g_setter(self):
+        self.moduleD.g = "g"
+
+        self.assertEqual(self.moduleD.g, "g")
