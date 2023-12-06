7af8f187a32abaa008218f9ca26336f7405c52d7
Jacob Feng <peanutChowder@gmail.com>
2023-12-05 22:56:23 -0700

    Complete test set up

--- tests_bottomup/TestRunner.py
+++ tests_bottomup/TestRunner.py
@@ -4,30 +4,30 @@
 import unittest
 
 # # import your tests modules (Apply integration testing method)
-# from tests import TestModuleA
-# from tests import TestModuleB
-# from tests import TestModuleC
-# from tests import TestModuleD
-# from tests import TestModuleE
-# from tests import TestModuleF
-# from tests import TestModuleG
+from tests_bottomup import TestModuleA
+from tests_bottomup import TestModuleB
+from tests_bottomup import TestModuleC
+from tests_bottomup import TestModuleD
+from tests_bottomup import TestModuleE
+from tests_bottomup import TestModuleF
+from tests_bottomup import TestModuleG
 
 
 
 # # initialize the tests suite
-# loader = unittest.TestLoader()
-# suite  = unittest.TestSuite()
+loader = unittest.TestLoader()
+suite  = unittest.TestSuite()
 
 # # add tests to the tests suite
-# suite.addTests(loader.loadTestsFromModule(TestModuleA))
-# suite.addTests(loader.loadTestsFromModule(TestModuleB))
-# suite.addTests(loader.loadTestsFromModule(TestModuleC))
-# suite.addTests(loader.loadTestsFromModule(TestModuleD))
-# suite.addTests(loader.loadTestsFromModule(TestModuleE))
-# suite.addTests(loader.loadTestsFromModule(TestModuleF))
-# suite.addTests(loader.loadTestsFromModule(TestModuleG))
+suite.addTests(loader.loadTestsFromModule(TestModuleA))
+suite.addTests(loader.loadTestsFromModule(TestModuleB))
+suite.addTests(loader.loadTestsFromModule(TestModuleC))
+suite.addTests(loader.loadTestsFromModule(TestModuleD))
+suite.addTests(loader.loadTestsFromModule(TestModuleE))
+suite.addTests(loader.loadTestsFromModule(TestModuleF))
+suite.addTests(loader.loadTestsFromModule(TestModuleG))
 
 
 # # initialize a runner, pass it your suite and run it
-# runner = unittest.TextTestRunner(verbosity=3)
-# result = runner.run(suite)
+runner = unittest.TextTestRunner(verbosity=3)
+result = runner.run(suite)
